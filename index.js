73f492400b9da1ea65b0188c8064ea8452146fa2
Not-LagsAlot <68179601+Not-LagsAlot@users.noreply.github.com>
2020-08-03 12:41:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -314,7 +314,6 @@ bot.on('message', async message=>{
     message.delete();
     message.channel.send(args.slice(1).join(" "));
     const emoji = message.guild.emojis.cache.find(emoji => emoji.name === 'thumbsup');
-  message.react(emoji)
   const emoji = message.guild.emojis.cache.find(emoji => emoji.name === 'thumbsdown');
   message.react(emoji);
 
