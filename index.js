0d1330fc692fadaeb839fd60b50110d39f4310b5
Not-LagsAlot <68179601+Not-LagsAlot@users.noreply.github.com>
2020-08-05 19:37:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const alexabot = require('alexa-bot-api')
 var version = 'v0.2';
 const ytdl = require('ytdl-core')
 const moment = require("moment");
@@ -10,7 +9,6 @@ var command = '.commands';
 const PREFIX = '.';
 var changes = 'Added 1 new command, Fixed bugs and crashes';
 var AI = 'AI currently in work';
-var chatbot = new alexabot("aw2plm")
 
 
 
@@ -34,16 +32,7 @@ bot.on('ready', () => {
 
 
 
-
-
 bot.on('message', async message => {
-
-  if(message.channel.id === '739055164425764965'){
-    if(message.author.bot) return;
-    let content = message.content;
-    chatbot.getReply(content).then(r => message.channel.send(r))
-  }
-  
   let args = message.content.slice(PREFIX.length).split(' ');
 
   if(message.content === 'hi'){
@@ -303,6 +292,9 @@ bot.on('message', async message => {
       break;
 
 
+      case 'ai':
+       message.guild.channels.create === '739055164425764965'
+      break;
 
 
 
