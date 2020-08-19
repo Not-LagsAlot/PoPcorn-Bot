79131ae6a94b8f21d91bd04628ca91958e44e54e
Not-LagsAlot <68179601+Not-LagsAlot@users.noreply.github.com>
2020-08-19 12:41:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -581,7 +581,41 @@ message.channel.send(args.join(' ').split('').map(c => mapping[c] || c).join('')
         if (number == 0) {
             message.channel.send('Tails')
         }
-      }
+      }else if(command === 'rps'){  if (!args[1]) {
+        return message.channel.send('Please include your choice.')
+    }
+
+    let choices = ['rock', 'paper', 'scissors'];
+    if (choices.includes((args[1]).toLowerCase())) {
+        let number = Math.floor(Math.random() * 3);
+        if (number == 1) {
+            return message.channel.send('It was a tie, we both had ' + (args[1]).toLowerCase())
+        }
+        if (number == 2) {
+            if ((args[1]).toLowerCase() == "rock") {
+                return message.channel.send('I won, I had paper.')
+            }
+            if ((args[1]).toLowerCase() == "paper") {
+                return message.channel.send('I won, I had scissors.')
+            }
+            if ((args[1]).toLowerCase() == "scissors") {
+                return message.channel.send('I won, I rock.')
+            }
+        }
+        if (number == 0) {
+            if ((args[1]).toLowerCase() == "rock") {
+                return message.channel.send('You won, I had scissors.')
+            }
+            if ((args[1]).toLowerCase() == "paper") {
+                return message.channel.send('You won, I had rock.')
+            }
+            if ((args[1]).toLowerCase() == "scissors") {
+                return message.channel.send('You won, I paper.')
+            }
+        }
+    } else {
+        return message.channel.send('Please include either: Rock, Paper, or Scissors.')
+    }}
       })
 
 
