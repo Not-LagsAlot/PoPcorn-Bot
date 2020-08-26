418a2689ff4de1473725407aa31483ae1cfa76bd
Not-LagsAlot <68179601+Not-LagsAlot@users.noreply.github.com>
2020-08-26 12:06:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -767,7 +767,12 @@ message.channel.send(format);
         message.channel.send(`${check[response]}`);
       }else if(command === 'invite'){
         const invite = new Discord.MessageEmbed()
-        .setDescription('**[Bot invite](https://discord.com/oauth2/authorize?client_id=723506760299839499&scope=bot&permissions=2146958847)**\n**[Support server](https://discord.gg/MJHfQ54)**')
+        const invite = new Discord.MessageEmbed()
+      .setTitle('Invite')
+      .setDescription(`**[Bot invite](https://discord.com/oauth2/authorize?client_id=723506760299839499&scope=bot&permissions=2146958847)**\n**[Support](https://discord.gg/MJHfQ54)**`)
+      .setColor('RANDOM')
+        
+        message.channel.send(invite);
       }
 
   })
