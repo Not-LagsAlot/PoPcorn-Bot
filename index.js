1d908155cbb4c7b6523c5c34690110cd1b2daddc
Not-LagsAlot <68179601+Not-LagsAlot@users.noreply.github.com>
2020-10-13 11:58:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1766,16 +1766,17 @@ message.channel.send(format);
 
   let every = client.db.all().filter(i => i.ID.startsWith("xp_")).sort((a, b) => b.data - a.data);
   let rank = every.map(x => x.ID).indexOf(`xp_${user.id}`) + 1;
-            const card = new canvacord.Rank()
-            .setUsername(user.username)
-            .setDiscriminator(user.discriminator)
-            .setRank(rank)
-            .setLevel(level)
-            .setCurrentXP(exp)
-            .setRequiredXP(neededXP)
-            .setStatus(user.presence.status)
-            .setAvatar(user.displayAvatarURL({ format: "png", size: 1024 }));
-        
+            // v4 rank card
+   let img = await canvacord.rank({
+    username: user.username,
+  discrim: user.discriminator,
+     currentXP: exp.toString(),
+     neededXP: neededXP.toString(),
+    rank: rank.toString(),
+     level: level.toString(),
+     avatarURL: user.displayAvatarURL({ format: "png" }),
+     background: "https://images.unsplash.com/photo-1523821741446-edb2b68bb7a0?ixlib=rb-1.2.1&w=1000&q=80"
+  });
           const img = await card.build();
           
           return message.channel.send(new MessageAttachment(img, "rank.png"));
