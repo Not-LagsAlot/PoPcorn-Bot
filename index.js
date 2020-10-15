c6ab84caf71c85548591df34b742be27780cec32
Not-LagsAlot <68179601+Not-LagsAlot@users.noreply.github.com>
2020-10-15 13:59:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1744,48 +1744,8 @@ message.channel.send(format);
           setTimeout(() => {
               usedCommand.delete(message.author.id);
           }, 5000); //You can set the ammount of the cooldown here! Its Formated to Miliseconds.
-          }else if(command === 'cc-create'){
-            if (!message.member.permissions.has("MANAGE_MESSAGES"))
-            return message.channel.send(`Hey! You don't have enough permissions`);
-          if (!args[0])
-            return message.channel.send(`Please give a name for your custom command`);
-          if (!args.slice(1).join(" "))
-            return message.channel.send(`No content given`);
-          custom.findOne(
-            { Guild: message.guild.id, Command: args[0] },
-            async (err, data) => {
-              if (err) throw err;
-              if (data) {
-                data.Content = args.slice(1).join(" ");
-                data.save();
-      
-                message.channel.send(
-                  `Successfully updated the command \`${args[0]}\``
-                );
-              } else if (!data) {
-                let newData = new custom({
-                  Guild: message.guild.id,
-                  Command: args[0],
-                  Content: args.slice(1).join(" "),
-                });
-                newData.save();
-                message.channel.send(
-                  `Successfully created the command \`${args[0]}\``
-                );
-              }
-            }
-          );
-        
-          }if(message.content.startsWith(`${prefix}`)){
-            custom.findOne(
-              { Guild: message.guild.id, command: cmd },
-              async (err, data) => {
-                if (err) throw err;
-                if (data) return message.channel.send(data.Content);
-                else return;
-              }
-            );
           }
+          
             
 
 
