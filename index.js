6532975337f7f2ad3d0ad94fa4de6eb50c36298a
LagsAlot <68179601+Not-LagsAlot@users.noreply.github.com>
2020-11-12 10:37:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2050,7 +2050,7 @@ message.channel.send(format);
     let text = args.join(' ')
     if(!text) return message.channel.send('No text is provided.')
     let img = await Canvacord.Canvas.changemymind(text);
-    let attachment = new MessageAttachment(img, 'changemymind.png');
+    let attachment = new Discord.MessageAttachment(img, 'changemymind.png');
     message.channel.send(attachment)
   }
 
